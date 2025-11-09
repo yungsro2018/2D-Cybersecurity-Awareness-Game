e4967cb440bd5a8e2c8512b19afe6a3e1fb25437
yungsro2018 <simanekaantindi@gmail.com>
2025-11-09 19:40:30 +0200

    Flag added and modifications on the SampleScene

---
+++ Assets/cyberTravel/Scripts/FinishPoint.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        { 
+            }
+    }
+}
