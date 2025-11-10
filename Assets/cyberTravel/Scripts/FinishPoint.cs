fe31998d1a054332891707b9f1b4483b2f2f5da6
yungsro2018 <simanekaantindi@gmail.com>
2025-11-10 12:54:00 +0200

    Transitions between Levels

--- Assets/cyberTravel/Scripts/FinishPoint.cs
+++ Assets/cyberTravel/Scripts/FinishPoint.cs
@@ -5,7 +5,8 @@ public class FinishPoint : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        { 
-            }
+        {
+            SceneController.instance.NextLevel();
+        }
     }
 }
